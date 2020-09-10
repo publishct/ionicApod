9fd7a1cfdaaf18ad71ecf584dab1c73cbaed7343
Cynthia Trujillo <developercynthia@gmail.com>
2020-09-10 11:10:12 -0500

    ionic on top of AngularCli NASA Apod

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,15 +2,9 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
-  },
-  {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full'
-  },
+  { path: '', redirectTo: 'apod', pathMatch: 'full' },
+  { path: 'apod', loadChildren: './apod/apod.module#ApodPageModule' },
+  { path: 'apod/:date', loadChildren: './apod/apod.module#ApodPageModule' },
 ];
 
 @NgModule({
